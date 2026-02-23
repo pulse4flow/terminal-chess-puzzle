23d5c8e3b11b53d4cb844917b005f6d2eae329c9
dvtzoe <ohmtzoe@gmail.com>
2026-02-23 18:29:09 +0700

    feat: simple lichess api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    dotenvy::dotenv().ok();
+
+    let body = reqwest::get("https://lichess.org/api/puzzle/next")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    println!("{body}");
 }
